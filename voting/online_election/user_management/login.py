c90510bbcf980080b1388daeb875da3cddf8f342
Padmesh Donthu <pd616769@dal.ca>
2021-03-31 18:23:28 -0300

    Javascript fix

--- voting/online_election/user_management/login.py
+++ voting/online_election/user_management/login.py
@@ -93,4 +93,5 @@ def logout():
     # remove the email from the session if it is present 
     session.pop('email_id', None)
     session.pop("role", None)
+    session.clear()
     return redirect(url_for("login.get_login_page"))
